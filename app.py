1b638c8a098d92d30b7271333c1848709f0d79a9
Devbrat Anuragi <a.devbrat@yahoo.com>
2023-03-11 18:36:21 +0530

    Moved PSQL to background thread, added button and formatted json report

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from dash import Dash, dcc, html, Input, Output, State
+from dash import Dash, dcc, html, Input, Output, State, DiskcacheManager
+import diskcache
 import dash_auth
 import dash_bootstrap_components as dbc
 import base64
+import dash
+import time
 import psql_conn
-
 
 import psycopg2
 from sshtunnel import SSHTunnelForwarder
@@ -18,7 +20,10 @@ VALID_USERNAME_PASSWORD_PAIRS = {
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+cache = diskcache.Cache("./cache")
+background_callback_manager = DiskcacheManager(cache)
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], background_callback_manager=background_callback_manager)
 auth = dash_auth.BasicAuth(
     app,
     VALID_USERNAME_PASSWORD_PAIRS
@@ -54,19 +59,39 @@ audio =  html.Audio(id = 'audioplayer',src='data:audio/mpeg;base64,{}'.format(en
 
 #----Accordian----#
 number_of_results = 0
+def render_report(report_obj):
+    return html.Div(
+        [
+            html.Ul([html.Li(str(key)+ " : " +str(value)
+    )for key,value in report_obj.items()])
+        ])
 
 def get_accordian_items(results):
-    
+
     return  dbc.Accordion([dbc.AccordionItem(
             [   html.Div("AUdio goes here"),
-                html.Div(str(res[1]))
+                html.Div(render_report(res[1]))
             ],
             title = f"Item {res[0]}",
         )for res in results])
-
 feteched_accordian = html.Div(
      id= 'fetched-audio-row'
 )
+
+fetch_button = html.Div(
+    [
+        html.Div(
+            [   html.P(id="paragraph_id", children=["Button not clicked"]),
+                html.Progress(id="progress_bar", value="0")
+
+            ]
+        ),
+        html.Div([
+        dbc.Button(id="button_id", children="Fetch Results", ),
+        dbc.Button(id="cancel_button_id", color = "danger", children="Cancel Running Job!"),],className="d-grid gap-2",)
+    ]
+)
+
 """
 fetched_row = dbc.Row(
     [   audio,
@@ -80,8 +105,8 @@ app.layout = dbc.Container([
 
     dbc.Row(
             [
-                dbc.Col([html.H2("Filter"),html.Br(), language_dropdown, state_dropdown, district_dropdown,category_dropdown], width = 4),
-                dbc.Col( [html.H2("Results" ),html.Br(),   feteched_accordian]),
+                dbc.Col([html.H2("Filter"),html.Br(), language_dropdown, state_dropdown, district_dropdown,category_dropdown,fetch_button], width = 4),
+                dbc.Col( [html.H2("Results" ),html.Br(),feteched_accordian]),
             ],
             align="start",
         ),
@@ -91,6 +116,55 @@ app.layout = dbc.Container([
 
 
 #----Load mor Audio----#
+
+@dash.callback(
+    #Output("paragraph_id", "children"),
+    Output('fetched-audio-row','children'),
+    Input("button_id", "n_clicks"),
+    State('language-dropdown','value'), 
+    State('state-dropdown','value'), 
+    State('district-dropdown','value'), 
+    State('category-dropdown','value'),
+    background=True,
+    running=[
+        (Output("button_id", "disabled"), True, False),
+        (Output("cancel_button_id", "disabled"), False, True),
+        (
+            Output("paragraph_id", "style"),
+            {"visibility": "hidden"},
+            {"visibility": "visible"},
+        ),
+        (
+            Output("progress_bar", "style"),
+            {"visibility": "visible"},
+            {"visibility": "hidden"},
+        ),
+    ],
+    cancel=Input("cancel_button_id", "n_clicks"),
+    #progress=[Output("progress_bar", "value"), Output("progress_bar", "max")],
+    prevent_initial_call=True
+)
+def update_progress(n_clicks,selected_language, selected_state,selected_district,selected_category):
+
+    print(selected_language, selected_state, selected_district, selected_category)
+    if selected_language is not None and selected_state is not None and selected_district is not None and selected_category is not None:
+        #set_progress((3, 5))
+ 
+        fetched_results = psql_conn.fetch_data( selected_state,selected_district,selected_language, selected_category)
+        #set_progress((5, 5))
+        print("result recived")
+        return [get_accordian_items(fetched_results)]
+
+    """
+    total = 5
+    for i in range(total + 1):
+        set_progress((str(i), str(total)))
+        time.sleep(1)
+
+    return f"Clicked {n_clicks} times"
+    """
+
+"""
 
 @app.callback(
     #Output('output','children'),
@@ -105,20 +179,14 @@ app.layout = dbc.Container([
     State('fetched-audio-row','children')
 )
 def more_output(selected_language, selected_state,selected_district,selected_category,old_row):
-    print(selected_language, selected_state, selected_district, selected_category)
+
     if selected_language is not None and selected_state is not None and selected_district is not None and selected_category is not None:
-        
-        if  old_row is None:
-            #session = psql_conn.get_database_connection()
-            fetched_results = psql_conn.fetch_data( selected_state,selected_district,selected_language, selected_category)
 
-            return [get_accordian_items(fetched_results)]
+        fetched_results = psql_conn.fetch_data( selected_state,selected_district,selected_language, selected_category)
 
-        #return old_row +[audio,html.Div("Report goes here")]
-        #print(psql_conn.fetch_data( selected_state,selected_district,selected_language, selected_category))
-        return old_row +[get_accordian_items(3)]
+        return [get_accordian_items(fetched_results)]
 """
-
+"""
 @app.callback(
     Output('output','children'),
     [Input('load-new-content','n_clicks')],
